52128493bd7d8f9108ca6341c5f408cbfe1873ae
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 18:27:50 +0900

    java中級編_6

---
+++ javalesson8.java
@@ -0,0 +1,12 @@
+public class javalesson8 {
+  public static void main(String[]ages){
+    int a = 2;
+    int b = 5;
+    System.out.println("交換前 : a="+a+" b="+b);
+
+  int x = a;
+  a = b;
+  b = x;
+  System.out.println("交換後 : a="+a+" b="+b);
+ }
+}

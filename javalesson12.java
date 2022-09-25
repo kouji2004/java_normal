2a4dfe82a39f2547a1659b6ed080e52657739746
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 19:05:30 +0900

    java中級編_9

--- javalesson12.java
+++ javalesson12.java
@@ -1,9 +1,9 @@
 public class javalesson12 {
   public static void main(String[]ages){
-    int mone = 820;
-    int hyaku = mone/100;
-    int zyuu = (mone-(hyaku*100))/10;
-    System.out.println("100円"+hyaku+"枚");
-    System.out.println("10円"+zyuu+"枚");
+int mone = 3480;
+int hyuku = mone/100;
+int jyuu = (mone-(hyuku*100))/10;
+System.out.println("100円"+hyuku+"が枚");
+System.out.println("10円が"+jyuu+"が枚");
   }
 }

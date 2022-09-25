160d150c0bd81a4601ef17cde80d4b45c2d0ca4d
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 13:30:14 +0900

    java中級編_1

--- javalesson.java
+++ javalesson.java
@@ -1,5 +1,11 @@
 public class javalesson{
   public static void main(String[]ages){
-    System.out.println("a");
+    for(int i=0; i<=100; i++){
+      if(i%2==0){
+        System.out.println("偶数");
+      }else{
+        System.out.println("奇数");
+      }
+    }
   }
 }

572f15dc716b2e5e66a765833cd13a30616a6306
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 13:36:47 +0900

    java中級編_2

---
+++ javalesson2.java
@@ -0,0 +1,10 @@
+public class javalesson2 {
+  public static void main(String[]ages){
+    int num = 23;
+    if(num > 0 && num <= 9){
+    System.out.println("一桁の整数は"+num+"です");
+    }else{
+      System.out.println(num+"は一桁の整数ではありません");
+    }
+  }
+}

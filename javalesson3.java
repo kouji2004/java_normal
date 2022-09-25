e09ccd3b107c2dcf21d81b43858c62dfda54bb10
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 13:41:45 +0900

    java中級編_3

---
+++ javalesson3.java
@@ -0,0 +1,12 @@
+public class javalesson3 {
+  public static void main(String[]ages){
+    int num1 = 2;
+    int num2 = 3;
+    int num3 = 4;
+    if(num1 <= num2 && num2<=num3){
+      System.out.println("ok");
+    }else{
+      System.out.println("ng");
+    }
+  }
+}
